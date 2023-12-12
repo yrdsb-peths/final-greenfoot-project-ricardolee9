be15b64d189665f527e68c93c7cdd6fffeeaba3b
Ricardo <440000735@gapps.yrdsb.ca>
2023-12-12 11:14:53 -0500

    Added Animation

--- Rocket.java
+++ Rocket.java
@@ -12,14 +12,61 @@ public class Rocket extends Actor
      * Act - do whatever the Rocket wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    // Game Frames Initialization
+    GreenfootImage[] idle = new GreenfootImage[8];
+    // Frames Image Index
+    int imageIndex = 0;
+    // Timer
+    SimpleTimer animationTimer = new SimpleTimer();
+    
+    /**
+     * Constructor for the Rocket class
+     */
+    
+    public Rocket() {
+        // Animation
+        for (int i = 0; i < idle.length; i++) {
+            idle[i] = new GreenfootImage("images/tile/tile00" + i + ".png");
+            idle[i].scale(100, 100);
+        }
+        // Mark Animation Timer
+        animationTimer.mark();
+        // Set Rocket Image
+        setImage(idle[0]);
+    }
+    
+    /**
+     * Rocket animation
+     */
+    private void animateRocket() {
+        // Change a frame every 100 ms
+        if (animationTimer.millisElapsed() < 100) {
+            return;
+        }
+        // Reset Timer
+        animationTimer.mark();
+        // Set animation image to next frame
+        setImage(idle[imageIndex]);
+        imageIndex = (imageIndex + 1) % idle.length;
+    }
+    
+    /**
+     * Sets the behaviour of the rocket
+     */
+    
     public void act()
     {
+        // Moves left
         if (Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left")) {
             moveLeft();
         }
+        // Moves right
         if (Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right")) {
             moveRight();
         }
+        // Animate Rocket
+        animateRocket();
     }
     
     /**
